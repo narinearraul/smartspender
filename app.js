66abf8142e37ddcd61dcd20e0722c6fb85e95e29
unknown <ncholaky@ucsd.edu>
2014-11-22 02:27:24 -0800

    added mongodb models

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+app.use(express.static(__dirname + '/public'));
+app.use( express.static( "public" ) );
+
+mongoose.connect('URL'); //establishing connection to the MongoDB Database currently running on ucsd server
+
+// models ===============================================
+
+var Chore = mongoose.model('Chore', {
+	name: String,
+	value: Number
+});
+
+var Child = mongoose.model('Child', {
+	username: String,
+	wishlist: {
+		itemName: String,
+		price: Number
+	}, 
+	chores: [
+		{
+			name: String,
+			value: Number,
+			status: String
+		}
+	], 
+});
+
+
+var Parent = mongoose.model('Parent', { 
+	username: String,
+	childId: mongoose.Schema.Types.ObjectId
+		
+});
+
+// routes ===============================================
+
+// Child
+app.post('/api/child/addItemToWishlist', function (req, res) {
+	Child.find(function(err, tokens) {
+				if(err)console.log(err);
+				
+				//res.send();
+				console.log("FOUND CHILD OBJECT");
+			  });
+})
+
+// Parent
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	console.log("NO CALL");
 })
 
 var server = app.listen(3000, function () {

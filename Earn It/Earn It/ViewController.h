619eda52b902b3ebd62655b11ec1d6b28a80384b
ncric8er <niteshanandan@yahoo.com>
2014-11-22 01:38:29 -0800

    Earn it added

---
+++ Earn It/Earn It/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Earn It
+//
+//  Created by Nitesh on 11/22/14.
+//  Copyright (c) 2014 Nitesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
